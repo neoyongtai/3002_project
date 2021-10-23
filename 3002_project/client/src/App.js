138ddde224d9a90af4e3858ac191b6c420bc9140
chexny <c.xueyao00@gmail.com>
2021-10-23 23:04:22 +0800

    Backend update

--- 3002_project/client/src/App.js
+++ 3002_project/client/src/App.js
@@ -4,6 +4,11 @@ import Dashboard from './components/DashBoard';
 import Individual from './components/Individual';
 import Notification from './components/Notifications';
 
+// set up
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 import { Route, BrowserRouter as Router } from "react-router-dom";
 
 const App = () => {
@@ -19,3 +24,15 @@ const App = () => {
 }
 
 export default App;
+
+/*
+const app = express();
+app.use(express.json());
+
+const port = 3001;
+const uri = process.env.MONGODB_CONNECTION_STR;
+
+app.listen(port, () => {
+  console.log('App is listening at http://localhost${port}')
+})
+*/
--- README.md
+++
@@ -1,3 +0,0 @@
-# 3002_project
-
-# This is the README of this project.
